4abd2c20b287f543433e993685657cc8d411bbcd
Mateusz Wedeł <mateusz.wedel@gmail.com>
2019-10-19 19:04:13 +0200

    Add test dir

---
+++ src/test/java/pl/wedel/szzti/TempTest.java
@@ -0,0 +1,13 @@
+package pl.wedel.szzti;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class TempTest {
+
+  @Test
+  public void name() {
+    assertThat(1).isEqualTo(1);
+  }
+}
